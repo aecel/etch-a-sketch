b968a2c8ac9b7d9839bac2329b24a7940afdb501
aecel <aecelacuna@gmail.com>
2022-07-24 23:43:07 +0800

    Add one square

---
+++ js-file.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("canvas");
+
+const square = document.createElement('div');
+square.classList.add('square');
+square.style.backgroundColor = 'red';
+square.style.height = '50px';
+square.style.width = '50px';
+
+container.appendChild(square);
