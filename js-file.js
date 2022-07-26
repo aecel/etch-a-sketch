f71d490dc551c0c2b3811c66fef7f16c8ac681a3
aecel <aecelacuna@gmail.com>
2022-07-26 16:58:36 +0800

    Add working canvas and way to change size of canvas

--- js-file.js
+++ js-file.js
@@ -1,9 +1,46 @@
-const container = document.getElementById("canvas");
+removeChildren = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-const square = document.createElement('div');
-square.classList.add('square');
-square.style.backgroundColor = 'red';
-square.style.height = '50px';
-square.style.width = '50px';
+makeCanvas = (num) => {
 
-container.appendChild(square);
+    for (i = 0; i < (num * num); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', darkenSquare);
+        canvas.appendChild(square);
+    }
+
+    canvas.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    canvas.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+}
+
+darkenSquare = (e) => {
+    e.target.style.backgroundColor = 'black';
+}
+
+changeCanvas = (e) => {
+    removeChildren(canvas);
+    makeCanvas(input.value);
+    console.log(`${input.value} is a ${typeof input.value}`);
+}
+
+// Initial canvas, not working
+// makeFirstCanvas = () => {
+//     let executed = false;
+//     return function () {
+//         if (!executed) {
+//             executed = true;
+//             makeCanvas(16);
+//         }
+//     };
+// }
+
+const canvas = document.getElementById("canvas");
+const button = document.getElementById("submit");
+const input = document.getElementById("input");
+button.addEventListener('click', changeCanvas);
+
+// makeFirstCanvas();
